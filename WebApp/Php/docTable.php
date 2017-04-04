064c88f64fa0860de86e33ee9284b997885e6f35
atzyrk200 <atzyrk200@caledonian.ac.uk>
2017-04-04 13:51:34 +0100

    Created table to view documents with distributee access and owners. creating both in single view was proving a pain in the ass

--- WebApp/Php/docTable.php
+++ WebApp/Php/docTable.php
@@ -5,7 +5,7 @@
     <input type="text" id="myInput" onkeyup="searchTable()">
     <div class="label-text">Search</div>
 </label>
-
+<h3>Author</h3>
 <table id="mainTable">
     <thead>
         <tr>
@@ -67,5 +67,82 @@
         }
         
         ?>
+    </tbody>
+</table>
+
+
+</br>
+</br>
+</br>
+<label>
+    <input type="text" id="myInput" onkeyup="searchTable()">
+    <div class="label-text">Search</div>
+</label>
+
+<h3>Distrebutees</h3>
+<table id="mainTable">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Title</th>
+        <th>Revision</th>
+        <th>Author ID</th>
+        <th style="min-width: 150px;">Comment</th>
+        <th>Status</th>
+        <th>Edit</th>
+        <th>Download</th>
+        <th>Delete</th>
+        <th>Activate</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <?php
+
+
+    $servername = "localhost";
+    $dbname = "mydb";
+
+    try {
+        $conn=new PDO("mysql:host=$servername;dbname=$dbname",DB_USER, DB_PASS);
+        $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo DB_USER . " is connected to " . $dbname;
+    } catch(PDOException $e) {
+        echo "Connection Failed: <br />" . $e -> getMessage();
+    }
+
+    $query= "SELECT document.id, user.username, document.name, document.comment, document.status,document.Author_id FROM document INNER JOIN distributee_access ON
+document.id = distributee_access.Document_id
+INNER JOIN user ON 
+distributee_access.User_id=user.id
+WHERE (user.username='{$_SESSION['username']}' AND (document.status = 'active' OR document.status = 'draft'))";
+
+    foreach ($conn->query($query) as $row) {
+        echo "
+                <tr id=" . "tableRow" . $row['id'] . " >
+                    <td> " . $row['id'] . " </td>
+                    <td> " . $row['name'] . " </td>
+                    <td> " . "REVISION" . " </td>
+                    <td> " . $row['Author_id'] . " </td>
+                    <td> " . $row['comment'] . " </td>
+                    <td> " . $row['status'] . " </td>
+                    <td> " . "<button class='btn small secondary'>
+                                <a class='deleteDoc' href=\"Php/editDoc.php?documentId=" . $row['id'] . "\"> Edit   
+                                <i class='fa fa-pencil-square-o'></i></a>
+                             </button>" . " </td>
+                    <td> " . "<button class='btn small primary'>Download <i class='fa fa-download'></i></button>" . " </td>
+                    <td> " . "<button class='btn small secondary'>
+                                <a class='deleteDoc' href=\"Php/deleteDoc.php?documentId=" . $row['id'] . "\"> Delete
+                                <i class='fa fa-trash'></i></a>
+                             </button>" . " </td>
+                             
+                    <td> " . "<button class='btn small secondary'>
+                                <a class='deleteDoc' href=\"Php/activateDoc.php?documentId=" . $row['id'] . "\"> Activate</a>
+                             </button>" . " </td>
+                </tr>
+            ";
+    }
+
+    ?>
     </tbody>
 </table>
